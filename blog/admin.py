5122195fed00667a4d107e131f9010704626b7d4
Julia <riyamtechcampus@gmail.com>
2022-02-04 04:08:55 -0800

    django report using import export mysqlclient and collectstatic

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
+from .models import Post, Comment
+from import_export.admin import ImportExportModelAdmin
 
-# Register your models here.
+# admin.site.register(Post)
+@admin.register(Post)
+class PostImportExport(ImportExportModelAdmin):
+    pass
 
 
-from .models import Post , Comment
-
-admin.site.register(Post)
-admin.site.register(Comment)
+# admin.site.register(Comment)
+@admin.register(Comment)
+class CommentImportExport(ImportExportModelAdmin):
+    pass
